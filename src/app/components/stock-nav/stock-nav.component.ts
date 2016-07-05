cbc77164ddce3188650ab345a98daa3a19f11e10
{{name}} <{{desk_login}}@desk.com>
2016-07-05 15:44:23 -0400

    Move template and style into nav component definition

--- src/app/components/stock-nav/stock-nav.component.ts
+++ src/app/components/stock-nav/stock-nav.component.ts
@@ -3,8 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'StockNav',
-  templateUrl: 'stock-nav.component.html',
-  styleUrls: ['stock-nav.component.css']
+  template: `
+    <div class="row">
+      <div class="col-md-12">
+        <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+               <span class="sr-only">Toggle navigation</span>
+               <span class="glyphicon glyphicon-minus"></span>
+            </button>
+          <a class="navbar-brand" href="#">Brand (included)</a>
+          </div>
+        </nav>
+      </div>
+    </div>`, 
+  styles: ['div {background: yellow;}', '.navbar-brand {font-size: 20px;}']    
 })
 export class StockNavComponent implements OnInit {
 
@@ -12,5 +25,4 @@ export class StockNavComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
